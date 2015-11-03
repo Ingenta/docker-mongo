b983d5fa497b031262fa29eb437498cbb43bd583
蒋凯 <kai.jiang@digitalpublishing.cn>
2015-11-03 10:02:32 +0800

    fixture for pacs

--- resource/entrypoint.sh
+++ resource/entrypoint.sh
@@ -6,8 +6,10 @@ then
 	mongo --shell /resource/setup.js
 	sleep 2
 	mongoimport -d meteor -c ip2country /resource/ip2country_fixture.txt
+	mongoimport -d meteor -c pacs /resource/pacs_fixture.txt
 	mongod --shutdown
 	rm /resource/setup.js
 	rm /resource/ip2country_fixture.txt
+	rm /resource/pacs_fixture
 fi
 mongod --replSet rs
